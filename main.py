3300895b85a79cfd5dd7ab0d40ed358b4876f7ed
Jared R. Mallas <jrmallas@icloud.com>
2018-07-04 09:08:33 -0500

    rename errantly added file

--- main.py
+++ main.py
@@ -6,30 +6,40 @@ import board
 import neopixel
  
 pixel_pin = board.D1
-num_pixels = 15
-SPEED = 2
+num_pixels = 32
+SPEED = 20000
  
-pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.3, auto_write=False)
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.3, auto_write=False, pixel_order=(1, 0, 2, 3))
 
 # COLORS 
-RED = (255, 0, 0)
-YELLOW = (255, 150, 0)
-ORANGE = (255,165,0)
-GREEN = (0, 255, 0)
-WHITE = (255, 255, 255)
-CYAN = (0, 255, 255)
-BLUE = (0, 0, 255)
-SKYBLUE = (135,206,250)
-PURPLE = (180, 0, 255)
-BLACK = (0, 0, 0)
-LOWWHITE = (4, 4, 4)
+RED = (255, 0, 0, 0)
+YELLOW = (255, 150, 0, 0)
+ORANGE = (255, 165, 0, 0)
+GREEN = (0, 255, 0, 0)
+WHITE = (255, 255, 255, 0)
+CYAN = (0, 255, 255, 0)
+BLUE = (0, 0, 255, 0)
+SKYBLUE = (135, 206, 250, 0)
+PURPLE = (180, 0, 255, 0)
+BLACK = (0, 0, 0, 0)
+INDIGO = (63, 81, 181, 0)
+VIOLET = (238, 130, 238, 0)
+LOWWHITE = (4, 4, 4, 4)
+LOWRED = (4, 0, 0, 0)
+LOWBLUE = (0, 0, 4, 0)
+
+flippity = LOWRED
 
 # COLOR PALLETS
 LIGHTNING = [WHITE, CYAN, SKYBLUE, BLUE, PURPLE]
 FIRE = [RED, YELLOW, BLACK, ORANGE]
+USA = [LOWRED, LOWWHITE, LOWBLUE, BLACK]
+CANADA = [LOWWHITE, LOWRED, BLACK]
+RAINBOW = [RED, ORANGE, YELLOW, GREEN, BLUE, INDIGO, VIOLET]
 
 # functions
 def burst(aPixel):
+    # print("  Entering function: burst")
     pixels[aPixel] = WHITE
     time.sleep(0.0125)
     pixels.show()
@@ -44,39 +54,63 @@ def burst(aPixel):
     # pixels.show()
 
 def palletBurst(aPixel, aPallet):
+    # print("  Entering function: palletBurst")
     # print('aPixel is ', aPixel)
     # print('aPallet is ', aPallet)
-    aColor = random.randint(0, len(aPallet) -1)
+    # aColor = random.randint(0, len(aPallet) -1)
+    aColor = random.randint(0, len(aPallet) - 1)
     # print('aColor is', aColor)
     pixels[aPixel] = aPallet[aColor]
     pixels.show()
-    time.sleep(0.0125)
+    # time.sleep(0.0125)
 
 def multiPalletBurst(aPixel, aPallet):
-    #total duration of burst
-    burstDuration = .125
+    # print("  Entering function: multiPalletBurst")
+    # total duration of burst
+    # burstDuration = .0625
     
-    #how many colors in the pallet?
+    # how many colors in the pallet?
     numColors = len(aPallet)
-    print('numColors is ', numColors)
-    miniBurst = burstDuration/numColors
-    print('  miniBurst is', miniBurst)
+    # print('numColors is ', numColors)
+    # miniBurst = burstDuration/numColors
+    # print('  miniBurst is', miniBurst)
     # for total number of colors in pallet, choose a random one
     for i in range(numColors):
         aColor = random.randint(0, numColors-1)
         pixels[aPixel] = aPallet[aColor]
         pixels.show()
-        time.sleep(burstDuration/numColors)
+        # time.sleep(miniBurst)
+    pixels[aPixel] = BLACK
+    pixels.show
 
+def manyMultiPalletBurst(aPallet):
+    numColors = len(aPallet)
+    
+    for aColor in range(numColors):
+        
+        for i in range(len(pixels)):
+            # select the "even" pixels
+            if(i % 2 == 0):
+                color = aPallet[random.randint(0, numColors-1)]
+            else:
+                color = aPallet[random.randint(0, numColors-1)]
+            
+            pixels[i] = color
+            # print("pixel ", i, "is set to ", color)
+            # time.sleep(.0625)
 
-#MAIN LOOP (near as I can tell)
+        # print("showing...")
+        pixels.show()
+        # time.sleep(.0078)
+    
+# MAIN LOOP (near as I can tell)
 while True:
-    calmTime = random.random() / SPEED
-    aStrike = random.randrange(num_pixels)
-    # burst(aStrike)
-    # palletBurst(aStrike, LIGHTNING)
-    multiPalletBurst(aStrike, LIGHTNING)
-
-    pixels.fill(LOWWHITE)
-    pixels.show()
-    time.sleep(calmTime)
+    # for anOrderedPixel in pixels:
+    #    pixelPos = random.randint(0, (num_pixels-1))
+    #    multiPalletBurst(pixelPos, USA)
+    #manyMultiPalletBurst(FIRE)
+    #manyMultiPalletBurst(CANADA)
+    #manyMultiPalletBurst(LIGHTNING)
+    manyMultiPalletBurst(USA)
+    #manyMultiPalletBurst(RAINBOW)
+    
